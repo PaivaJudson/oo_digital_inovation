859021e2341e173903d14a19129ec906e24f1038
Judson Paiva <judson.paiva@hotmail.com>
2021-07-27 04:36:11 +0100

    Enum como classe e objectos

--- src/main/kotlin/one/digitalinovation/digionebank/teste/TesteEnum.kt
+++ src/main/kotlin/one/digitalinovation/digionebank/teste/TesteEnum.kt
@@ -5,11 +5,15 @@ import one.digitalinovation.digionebank.ClienteTipo
 fun main() {
 
     ClienteTipo.values().forEach {
-        elemento -> println("${elemento.name} -> ${elemento.toString()}")
+        elemento -> println("${elemento.name} -> ${elemento.descricao}")
     }
 
     ClienteTipo.values().forEach {
-        println("${it.name} -> ${it}")
+        println("${it.name} -> ${it.descricao}")
     }
+
+    val obj = ClienteTipo.PF
+
+    println(ClienteTipo.printDescricao())
 
 }
