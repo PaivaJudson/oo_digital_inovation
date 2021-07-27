8a9364ff7eea5b15326001b44fcd1ab208dacf20
Judson Paiva <judson.paiva@hotmail.com>
2021-07-27 03:25:44 +0100

    classe primárias vs classe secundaria, métodos ou acções

--- src/main/kotlin/one/digitalinovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinovation/digionebank/Pessoa.kt
@@ -3,15 +3,19 @@ package one.digitalinovation.digionebank
 class Pessoa {
 
     var nome: String = "Judson"
-
     var cpf: String = "123.123.123-11"
     private set
 
+    constructor()
+
+    fun pessoaInfo() = "Nome = $nome e CPF = $cpf"
+
 }
+
 
 fun main() {
     val person = Pessoa()
 
-    println("Nome = ${person.nome}; CPF = ${person.cpf}")
+    println("${person.pessoaInfo()}")
 
 }
