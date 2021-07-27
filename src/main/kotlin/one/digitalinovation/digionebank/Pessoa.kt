1ce0d208544aaa2584a9c69215dd908be7f60613
Judson Paiva <judson.paiva@hotmail.com>
2021-07-27 02:40:35 +0100

    usando modificador de acesso private só para o set

--- src/main/kotlin/one/digitalinovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinovation/digionebank/Pessoa.kt
@@ -3,11 +3,9 @@ package one.digitalinovation.digionebank
 class Pessoa {
 
     var nome: String = "Judson"
-    var cpf: String = "123.123.123-11"
 
-    inner class Endereco{
-        var rua: String = "Rua C7"
-    }
+    var cpf: String = "123.123.123-11"
+    private set
 
 }
 
@@ -15,5 +13,5 @@ fun main() {
     val person = Pessoa()
 
     println("Nome = ${person.nome}; CPF = ${person.cpf}")
-    println("Endereco = ${person.Endereco().rua}")
+
 }
