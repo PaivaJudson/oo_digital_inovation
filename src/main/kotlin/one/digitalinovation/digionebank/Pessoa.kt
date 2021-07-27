82f6b8c7bf42ab04e4e8164dae3bfd9d2e6f15b4
Judson Paiva <judson.paiva@hotmail.com>
2021-07-27 02:26:14 +0100

    usando o inner class ou seja a classe interna

--- src/main/kotlin/one/digitalinovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinovation/digionebank/Pessoa.kt
@@ -5,10 +5,15 @@ class Pessoa {
     var nome: String = "Judson"
     var cpf: String = "123.123.123-11"
 
+    inner class Endereco{
+        var rua: String = "Rua C7"
+    }
+
 }
 
 fun main() {
     val person = Pessoa()
 
     println("Nome = ${person.nome}; CPF = ${person.cpf}")
+    println("Endereco = ${person.Endereco().rua}")
 }
